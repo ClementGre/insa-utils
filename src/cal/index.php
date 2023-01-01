cfe12597bda5585a5610d0c8c71f9e3d1aa84b3c
Clément Grennerat <clement.grennerat@free.fr>
2023-01-01 20:18:17 +0100

    [cal] Add settings feature
    Il est désormais possible de renommer automatiquement ou pas les évènements vers des noms encore plus naturels.
    Il est aussi possible de configurer l'affichage de la salle de cours dans le non de l'évènement.
    
    Took 55 minutes

--- src/cal/index.php
+++ src/cal/index.php
@@ -8,6 +8,10 @@ $desc = "Cette passerelle permet de convertir ton calendrier INSA en renommant l
 
 $url = '';
 if(isset($_GET['url'])) $url = urldecode($_GET['url']);
+$mode = '';
+if(isset($_GET['mode'])) $mode = urldecode($_GET['mode']);
+$room = '';
+if(isset($_GET['room'])) $room = urldecode($_GET['room']);
 
 ?>
 
@@ -32,19 +36,28 @@ if(isset($_GET['url'])) $url = urldecode($_GET['url']);
                 URL de ton calendrier ADE (lien abonnement iCal) :
                 <br>
                 <input class="url-input" type="text" name="url" value="<?= $url ?>" placeholder="https://ade-outils.insa-lyon.fr/ADE-Cal:~jgarzer!2022-2023:459877899af69B3D">
+                <br><br>
+                Mode d'affichage des évènements :
                 <br>
-                <br>
+                <select name="mode">
+                    <option value="0" <?= $mode == 0 ? 'selected' : '' ?>>Nom complet (CM Maths, TD Physique..)</option>
+                    <option value="1" <?= $mode == 1 ? 'selected' : '' ?>>Nom acronyme simplifié (CM MA, TD PH...)</option>
+                    <option value="2" <?= $mode == 2 ? 'selected' : '' ?>>Nom acronyme officiel (CM MA-AP, TD PH-AMP...)</option>
+                </select>
+                <br><br>
+                <input type="checkbox" name="room" value="true" <?= $room ? 'checked' : '' ?>> Afficher le nom de la salle dans le nom des évènements.
+                <br><br>
                 <input type="submit" value="Valider">
             </label>
         </form>
 
         <?php
         if(isset($_GET['url'])){
-            $url = 'https://insa-utils.live/cal/get.php?url=' . $url;
+            $url = 'https://insa-utils.live/cal/get.php?url=' . $url . '&mode=' . $mode . '&room=' . $room;
             ?>
             <p>
                 URL de ton calendrier convertis (nouveau abonnement iCal) :<br>
-                <span><?php echo '<a href="' . $url . '">' . $url . '</a>'; ?></span>
+                <span style="word-break: break-all"><?php echo '<a href="' . $url . '">' . $url . '</a>'; ?></span>
             </p>
             <?php
         }
