0cff62e7cb6eebd460fae7e9a802b66920ea6859
Clément Grennerat <clement.grennerat@free.fr>
2023-02-18 11:03:34 +0100

    [todo] Add fast links buttons to todos page
    Took 23 minutes

--- src/todo/views/inc/class.php
+++ src/todo/views/inc/class.php
@@ -28,88 +28,117 @@ $subjects = get_class_subjects($status['class_id']);
 
 ?>
 
-<div class="root-path-container" data-root-path="<?= htmlspecialchars(getRootPath()) ?>"></div>
-<div class="subjects-container" data-subjects="<?= htmlspecialchars(json_encode($subjects)) ?>"></div>
-<div class="csrf-container" data-csrf="<?= htmlspecialchars(gen_csrf_key('js')) ?>"></div>
-<div class="user-id-container" data-user-id="<?= htmlspecialchars($status['id']) ?>"></div>
+    <div class="root-path-container" data-root-path="<?= htmlspecialchars(getRootPath()) ?>"></div>
+    <div class="subjects-container" data-subjects="<?= htmlspecialchars(json_encode($subjects)) ?>"></div>
+    <div class="csrf-container" data-csrf="<?= htmlspecialchars(gen_csrf_key('js')) ?>"></div>
+    <div class="user-id-container" data-user-id="<?= htmlspecialchars($status['id']) ?>"></div>
 
 <?php
 if (isset($_SESSION['errors'])) {
     print_errors_html($_SESSION['errors']);
     $_SESSION['errors'] = array();
 }
+
+$q = getDB()->prepare('SELECT COUNT(*) FROM users WHERE requested_class_id = :requested_class_id AND class_id IS NULL');
+$q->execute(['requested_class_id' => $status['class_id']]);
+$row = $q->fetch();
+$count = $row ? $row[0] : 0;
 ?>
 
-<h3>À faire :</h3>
-<div class="todo-list">
-    <?php
-    if (!print_todos($to_do, $subjects)) {
-        ?>
-        <p class="no-todo">Aucune tâche pour le moment !</p>
+    <h3>À faire :</h3>
+    <div class="todo-list">
         <?php
-    }
-    ?>
-</div>
-
-<h3>Ajouter :</h3>
-<div class="todo-list">
-    <?php
-    if (count($subjects) != 0) {
+        if (!print_todos($to_do, $subjects)) {
+            ?>
+            <p class="no-todo">Aucune tâche pour le moment !</p>
+            <?php
+        }
         ?>
-        <form class="todo" method="post" action="<?= getRootPath() ?>todo/manage">
-            <?php set_csrf() ?>
-            <input type="hidden" name="action" value="add"/>
-            <div class="heading">
-                <select id="subject" name="subject_id" required onchange="onSubjectComboChange(event);">
-                    <?php
-                    foreach ($subjects as $subject) {
-                        ?>
-                        <option value="<?= $subject['id'] ?>" <?= (($_POST['subject_id'] ?? '') === $subject['id']) ? 'selected="selected"' : '' ?>><?= out($subject['name']) ?></option>
+    </div>
+
+    <h3>Ajouter :</h3>
+    <div class="todo-list">
+        <?php
+        if (count($subjects) != 0) {
+            ?>
+            <form class="todo" method="post" action="<?= getRootPath() ?>todo/manage">
+                <?php set_csrf() ?>
+                <input type="hidden" name="action" value="add"/>
+                <div class="heading">
+                    <select id="subject" name="subject_id" required onchange="onSubjectComboChange(event);">
                         <?php
-                    }
-                    ?>
-                    <option value="manage">Gérer les matières</option>
-                </select>
-                <input type="date" id="duedate" name="duedate"
-                       value="<?= $_POST['duedate'] ?? date_in_a_week() ?>" min="<?= current_date() ?>"
-                       max="<?= year() . '-06-30' ?>" required>
-                <select class="fixed" id="type" name="type" required>
-                    <option value="report" <?= ($_POST['type'] ?? '') === 'report' ? 'selected="selected"' : '' ?>>
-                        Rendu
-                    </option>
-                    <option value="practice" <?= (!isset($_POST['type']) || $_POST['type'] === 'practice') ? 'selected="selected"' : '' ?>>
-                        Exercice
-                    </option>
-                    <option value="reminder" <?= ($_POST['type'] ?? '') === 'reminder' ? 'selected="selected"' : '' ?>>
-                        Pense bête
-                    </option>
-                </select>
-            </div>
-            <div class="content">
+                        foreach ($subjects as $subject) {
+                            ?>
+                            <option value="<?= $subject['id'] ?>" <?= (($_POST['subject_id'] ?? '') === $subject['id']) ? 'selected="selected"' : '' ?>><?= out($subject['name']) ?></option>
+                            <?php
+                        }
+                        ?>
+                        <option value="manage">Gérer les matières</option>
+                    </select>
+                    <input type="date" id="duedate" name="duedate"
+                           value="<?= $_POST['duedate'] ?? date_in_a_week() ?>" min="<?= current_date() ?>"
+                           max="<?= year() . '-06-30' ?>" required>
+                    <select class="fixed" id="type" name="type" required>
+                        <option value="report" <?= ($_POST['type'] ?? '') === 'report' ? 'selected="selected"' : '' ?>>
+                            Rendu
+                        </option>
+                        <option value="practice" <?= (!isset($_POST['type']) || $_POST['type'] === 'practice') ? 'selected="selected"' : '' ?>>
+                            Exercice
+                        </option>
+                        <option value="reminder" <?= ($_POST['type'] ?? '') === 'reminder' ? 'selected="selected"' : '' ?>>
+                            Pense bête
+                        </option>
+                    </select>
+                </div>
+                <div class="content">
         <textarea name="content" rows="4"
                   placeholder="Titre&#10;Description"><?= out($_POST['content'] ?? '') ?></textarea>
-            </div>
-            <div class="validate">
-                <input type="text" name="link" placeholder="Lien" value="<?= out($_POST['link'] ?? '') ?>">
-                <select class="fixed" name="visibility" required>
-                    <option value="public" <?= (!isset($_POST['visibility']) || $_POST['visibility'] === 'public') ? 'selected="selected"' : '' ?>>
-                        Publique
-                    </option>
-                    <option value="private" <?= (($_POST['visibility'] ?? '') === 'private') ? 'selected="selected"' : '' ?>>
-                        Privé
-                    </option>
-                </select>
-                <input class="fixed" type="submit" name="submit" value="Ajouter">
-            </div>
-        </form>
-        <?php
-    }else{
+                </div>
+                <div class="validate">
+                    <input type="text" name="link" placeholder="Lien" value="<?= out($_POST['link'] ?? '') ?>">
+                    <select class="fixed" name="visibility" required>
+                        <option value="public" <?= (!isset($_POST['visibility']) || $_POST['visibility'] === 'public') ? 'selected="selected"' : '' ?>>
+                            Publique
+                        </option>
+                        <option value="private" <?= (($_POST['visibility'] ?? '') === 'private') ? 'selected="selected"' : '' ?>>
+                            Privé
+                        </option>
+                    </select>
+                    <input class="fixed" type="submit" name="submit" value="Ajouter">
+                </div>
+            </form>
+            <?php
+        } else {
+            ?>
+            <p class="no-todo">Pour ajouter une tâche, ajoutez d'abord des matières&#8239;:<br><a
+                        href="<?= getRootPath() ?>todo/subjects">Ajouter des matières</a></p>
+            <?php
+        }
         ?>
-        <p class="no-todo">Pour ajouter une tâche, ajoutez d'abord des matières&#8239;:<br><a href="<?= getRootPath() ?>todo/subjects">Ajouter des matières</a></p>
+    </div>
+    <div class="fast-links">
         <?php
-    }
-    ?>
-</div>
+        if ($count == 1) {
+            ?>
+            <a class="fast-link requests" href="<?= getRootPath() ?>todo/requests">
+                <p><?= $count ?> personne souhaite rejoindre votre classe</p>
+            </a>
+            <?php
+        }else if ($count != 0) {
+            ?>
+            <a class="fast-link requests" href="<?= getRootPath() ?>todo/requests">
+                <p><?= $count ?> personnes souhaitent rejoindre votre classe</p>
+            </a>
+            <?php
+        }
+        ?>
+        <a class="fast-link" href="<?= getRootPath() ?>todo/subjects">
+            <p>Gestion des matières</p>
+        </a>
+        <a class="fast-link" href="<?= getRootPath() ?>todo/all">
+            <p>Toutes les tâches</p>
+        </a>
+    </div>
 <?php
 if (count($done) != 0) {
     ?>
