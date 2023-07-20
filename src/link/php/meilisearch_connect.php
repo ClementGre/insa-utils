94ac021cf76fd2b0d9dfcbe7f3971798e9b9ce18
Clément Grennerat <clement.grennerat@free.fr>
2023-07-20 19:53:03 +0200

    [link] Fix host issue when working on server.
    Took 9 minutes

--- src/link/php/meilisearch_connect.php
+++ src/link/php/meilisearch_connect.php
@@ -3,5 +3,9 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 use Meilisearch\Client;
 
 global $client;
-$client = new Client('http://93.90.202.93:7700', getenv("MEILISEARCH_KEY"));
+$host = '127.0.0.1';
+if (str_starts_with($_SERVER['HTTP_HOST'], 'localhost')) {
+    $host = 'pdf4teachers.org';
+}
+$client = new Client('http://' . $host . ':7700', getenv("MEILISEARCH_KEY"));
 
