a194556ec661964932fef19fb2dddc037b857dfc
Clément Grennerat <clement.grennerat@free.fr>
2023-10-09 13:30:15 +0200

    [menu] Adding notifications try-catch

--- workers/menu/menu_notifications.py
+++ workers/menu/menu_notifications.py
@@ -59,60 +59,69 @@ def send_notifications_for_meal(day_menu, is_lunch, time_string):
 def send_notifications_content(title, text, is_ri, is_lunch, is_weekend, time_string):
     if time_string == '11:10' or time_string == '17:10':
         print("Sending ntfy.sh notifications...")
-        headers = {
-            "Title": title + f" - {datetime.date.today().day}/{datetime.date.today().month}",
-            "Tags": "plate_with_cutlery",
-            "Actions": "view, Voir menu, https://menu-restaurants.insa-lyon.fr/, clear=true"
-        }
-        if text == '':
-            requests.post("https://ntfy.sh/menu-insa",
-                          headers=headers)
-        else:
-            requests.post("https://ntfy.sh/menu-insa",
-                          data=text.encode(encoding='utf-8'),
-                          headers=headers)
+        try:
+            headers = {
+                "Title": title + f" - {datetime.date.today().day}/{datetime.date.today().month}",
+                "Tags": "plate_with_cutlery",
+                "Actions": "view, Voir menu, https://menu-restaurants.insa-lyon.fr/, clear=true"
+            }
+            if text == '':
+                requests.post("https://ntfy.sh/menu-insa",
+                              headers=headers)
+            else:
+                requests.post("https://ntfy.sh/menu-insa",
+                              data=text.encode(encoding='utf-8'),
+                              headers=headers)
+        except Exception as e:
+            print("Unable to send ntfy.sh notifications:", e)
 
     print("Sending webpush notification...")
 
-    db = db_connect()
-    cursor = db.cursor()
+    try:
+        db = db_connect()
+        cursor = db.cursor()
 
-    cursor.execute("""SELECT * FROM menu_subscriptions WHERE
-                    (lunch_time = %s OR dinner_time = %s)
-                    AND (
-                        (olivier = True AND olivier = %s)
-                        OR
-                        (
+        cursor.execute("""SELECT * FROM menu_subscriptions WHERE
+                        (lunch_time = %s OR dinner_time = %s)
+                        AND (
+                            (olivier = True AND olivier = %s)
+                            OR
                             (
-                                (ri_lunch = True AND ri_lunch = %s)
-                                OR
-                                (ri_dinner = True AND ri_dinner = %s)
+                                (
+                                    (ri_lunch = True AND ri_lunch = %s)
+                                    OR
+                                    (ri_dinner = True AND ri_dinner = %s)
+                                )
+                                AND (ri_weekend = True OR ri_weekend = %s)
                             )
-                            AND (ri_weekend = True OR ri_weekend = %s)
-                        )
-                    )""",
-                   (time_string, time_string,
-                    not is_ri,
-                    is_ri and is_lunch,
-                    is_ri and not is_lunch,
-                    is_ri and is_weekend))
+                        )""",
+                       (time_string, time_string,
+                        not is_ri,
+                        is_ri and is_lunch,
+                        is_ri and not is_lunch,
+                        is_ri and is_weekend))
 
 
 
-    columns = cursor.description
-    res = [{columns[index][0]: column for index, column in enumerate(value)} for value in cursor.fetchall()]
-    for sub in res:
+        columns = cursor.description
+        res = [{columns[index][0]: column for index, column in enumerate(value)} for value in cursor.fetchall()]
+        for sub in res:
+            try:
+                pywebpush.webpush(
+                    {
+                        "endpoint": sub['endpoint'],
+                        "keys": {"auth": sub['key_auth'], "p256dh": sub['key_p256dh']}
+                    },
+                    json.dumps({
+                        "title": title,
+                        "body": text,
+                        "is_olivier": not is_ri
+                    }),
+                    vapid_private_key=get_secrets()['webpush']['privateKey'],
+                    vapid_claims={"sub": "mailto:clement.grennerat@free.fr'}"}
+                )
+            except Exception as e:
+                print("Unable to send individual webpush notifications:", e)
 
-        pywebpush.webpush(
-            {
-                "endpoint": sub['endpoint'],
-                "keys": {"auth": sub['key_auth'], "p256dh": sub['key_p256dh']}
-            },
-            json.dumps({
-                "title": title,
-                "body": text,
-                "is_olivier": not is_ri
-            }),
-            vapid_private_key=get_secrets()['webpush']['privateKey'],
-            vapid_claims={"sub": "mailto:clement.grennerat@free.fr'}"}
-        )
+    except Exception as e:
+        print("Unable to send webpush notifications:", e)
