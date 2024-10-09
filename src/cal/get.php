1f501ea537f86ffdac46bd3d8281bde5fd35957d
adesnot <anatole.desnot@insa-lyon.fr>
2024-10-09 10:32:42 +0200

    Ajout de IF, GI, et HUMA (+distinction avec FIMI)

--- src/cal/get.php
+++ src/cal/get.php
@@ -6,14 +6,18 @@ use ICal\ICal;
 function strstr_after($string, $needle): string
 {
     $result = substr(strstr($string, $needle), strlen($needle));
-    if ($result) return $result;
+    if ($result) {
+        return $result;
+    }
     return $string; // Return $string if needle not found
 }
 
 function strstr_before($string, $needle): string
 {
     $result = strstr($string, $needle, true);
-    if ($result) return $result;
+    if ($result) {
+        return $result;
+    }
     return $string; // Return $string if needle not found
 }
 
@@ -30,17 +34,22 @@ function get_after_last_occurrence_of($string, $needle): string
 function convertCalendar($url, $mode, $room): void
 {
     try {
-        $ical = new ICal('ICal.ics', array(
-            'defaultSpan' => 2,     // Default value
-            'defaultTimeZone' => 'UTC',
-            'defaultWeekStart' => 'MO',  // Default value
-            'disableCharacterReplacement' => false, // Default value
-            'filterDaysAfter' => null,  // Default value
-            'filterDaysBefore' => null,  // Default value
-            'httpUserAgent' => null,  // Default value
-            'skipRecurrence' => false, // Default value
-        ));
-        $ical->initUrl($url, $username = null, $password = null, $userAgent = null);
+        $ical = new ICal("ICal.ics", [
+            "defaultSpan" => 2, // Default value
+            "defaultTimeZone" => "UTC",
+            "defaultWeekStart" => "MO", // Default value
+            "disableCharacterReplacement" => false, // Default value
+            "filterDaysAfter" => null, // Default value
+            "filterDaysBefore" => null, // Default value
+            "httpUserAgent" => null, // Default value
+            "skipRecurrence" => false, // Default value
+        ]);
+        $ical->initUrl(
+            $url,
+            $username = null,
+            $password = null,
+            $userAgent = null
+        );
 
         header("Content-type:text/calendar");
         header("Content-Disposition:attachment;filename=edt_insa.ics");
@@ -56,7 +65,6 @@ function convertCalendar($url, $mode, $room): void
         }
 
         echo "END:VCALENDAR\r\n";
-
     } catch (\Exception $e) {
         die($e);
     }
@@ -67,19 +75,26 @@ function editEventAndPrint($event, $mode, $room)
     $subject = strstr_between($event->description, "] ", "\n("); // Full name
     $classDetails = strstr_between($event->description, "\n(", ")\n");
 
-    $firstExplodedSummary = explode("::", $event->summary);  // Exploding  FIMI:2:S1::MA-TF:TD::048 #011
+    $firstExplodedSummary = explode("::", $event->summary); // Exploding  FIMI:2:S1::MA-TF:TD::048 #011 into [FIMI:2:S1, MA-TF:TD, 048]
     $explodedSummary = [];
+    $explodedFormationDetails = [];
+
     if (count($firstExplodedSummary) >= 2) {
         $explodedSummary = explode(":", $firstExplodedSummary[1]); // [MA-TF, TD]
+        $explodedFormationDetails = explode(":", $firstExplodedSummary[0]); // [FIMI, 2, S1]
     }
 
-    $subjectTag = count($explodedSummary) >= 1 ? $explodedSummary[0] : ""; // FIMI:2:S1::MA-TF:TD::048 #011
+    $formation = count count($explodedFormationDetails) >= 2 ? $explodedFormationDetails[0] : ""; // FIMI, IF, GI
+    $subjectTag = count($explodedSummary) >= 1 ? $explodedSummary[0] : ""; // MA-TF
     $type = count($explodedSummary) >= 2 ? $explodedSummary[1] : null; // CM, TD, TP, EV => IE, EDT => Autre
 
-
-    if ($subjectTag === "SOU" || $subjectTag === "LV"
-        || ($subjectTag == "EPS" && $type == "EDT")
-        || ($subjectTag == "*" && $type == "EDT" && $classDetails == "Créneau P2i")) { // Matières à ne pas afficher : Créneaux Soutien, Langues et Sport
+    if (
+        $subjectTag === "SOU" ||
+        $subjectTag === "LV" ||
+        ($subjectTag == "EPS" && $type == "EDT") ||
+        ($subjectTag == "*" && $type == "EDT" && $classDetails == "Créneau P2i")
+    ) {
+        // Matières à ne pas afficher : Créneaux Soutien, Langues et Sport
         return;
     }
     $p2i_number = "";
@@ -88,7 +103,9 @@ function editEventAndPrint($event, $mode, $room)
         $subjectTag = "P2I"; // P2I2-TF-SH2 => P2I
     }
 
-    if ($type == "EDT") $type = null;
+    if ($type == "EDT") {
+        $type = null;
+    }
 
     // Location
 
@@ -101,53 +118,122 @@ function editEventAndPrint($event, $mode, $room)
             $fullLocation = null;
         }
     } else {
-        $location = join(", ", array_map(function ($loc) {
-            $room = strstr_before($loc, " (");
-            $room = get_after_last_occurrence_of($room, " - ");
+        $location = join(
+            ", ",
+            array_map(function ($loc) {
+                $room = strstr_before($loc, " (");
+                $room = get_after_last_occurrence_of($room, " - ");
 
-            if (str_starts_with($room, "Amphithéâtre")) {
-                $amphiExploded = explode(" ", $room);
-                if (count($amphiExploded) >= 3) {
-                    return "Amphi " . $amphiExploded[count($amphiExploded) - 1]; // Takes only the last word : William Hamilton => Hamilton
+                if (str_starts_with($room, "Amphithéâtre")) {
+                    $amphiExploded = explode(" ", $room);
+                    if (count($amphiExploded) >= 3) {
+                        return "Amphi " .
+                            $amphiExploded[count($amphiExploded) - 1]; // Takes only the last word : William Hamilton => Hamilton
+                    }
+                    array_shift($amphiExploded);
+                    return "Amphi " . join(" ", $amphiExploded);
                 }
-                array_shift($amphiExploded);
-                return "Amphi " . join(" ", $amphiExploded);
-            }
-            return $room;
-        }, explode(",", $event->location)));
+                return $room;
+            }, explode(",", $event->location))
+        );
 
-        $fullLocation = join(" / ", array_map(function ($loc) {
-            $fullRoom = strstr_after($loc, " - ");
-            return str_replace("Amphithéâtre", "Amphi", $fullRoom);
-        }, explode(",", $event->location)));
+        $fullLocation = join(
+            " / ",
+            array_map(function ($loc) {
+                $fullRoom = strstr_after($loc, " - ");
+                return str_replace("Amphithéâtre", "Amphi", $fullRoom);
+            }, explode(",", $event->location))
+        );
     }
-
 
     // Modes : 0 = full name, 1 = short, 2 = default
     if ($mode != 2) {
-        $subjectTag = explode("-", $subjectTag)[0];
-        if ($mode != 1) { // Default, Human full readable names
-            $subjectTag = match ($subjectTag) {
-                "PH" => "Physique",
-                "MA" => "Maths",
-                "CO", "CP" => "Conception",
-                "CH" => "Chimie",
-                "TH" => "Thermo",
-                "MS" => "Méca",
-                "ANG" => "Anglais",
-                "EPS" => "Sport",
-                "P2I" => "P2I" . $p2i_number . " - " . $classDetails,
-                "*" => $subject,
-                default => $subjectTag
-            };
-        }else if($subjectTag == "P2I") {
+        $subjectTag = explode("-", $subjectTag)[0]; // explodes from MA-TF to [MA, TF]
+        if ($mode != 1) {
+            // Default, Human full readable names
+            if ($formation == "FIMI") {
+                $subjectTag = match ($subjectTag) {
+                    "PH" => "Physique",
+                    "MA" => "Maths",
+                    "CO", "CP" => "Conception",
+                    "CH" => "Chimie",
+                    "TH" => "Thermo",
+                    "MS" => "Méca",
+                    "ANG" => "Anglais",
+                    "EPS" => "Sport",
+                    "P2I" => "P2I" . $p2i_number . " - " . $classDetails,
+                    "*" => $subject,
+                    default => $subjectTag,
+                };
+            } else if ($formation == "IF") {
+                $subjectTag = match ($subjectTag) {
+                    "AC" => "Architecture des circuits numériques",
+                    "AO" => "Architecture des ordinateurs",
+                    "PRC" => "Programmation en C",
+                    "ALGO" => "Algorithmes et structures de données",
+                    "POO1" => "Programmation Orientée Objet - C++ - Les Bases",
+                    "POO2" => "Programmation Orientée Objet - C++ - Avancée",
+                    "OP" => "Outils de Programmation",
+                    "CMSI" => "Calcul matriciel et synthèse d'images",
+                    "TSI" => "Traitement du Signal et des Images",
+                    "MD" => "Modélisation des données",
+                    "MP" => "Modélisation des Processus",
+                    "BDR" => "Système de gestion de base de données",
+                    "SHC1" => "Sciences Humaines et Communication",
+                    "PPP1" => "Projet Personnel et Professionnel",
+                    "LV1" => "Langue Vivante 1",
+                    "LV2" => "Langue Vivante 2",
+                    "EPS" => "Éducation Physique et Sportive",
+                    "*" => $subject,
+                    default => $subjectTag,
+                };
+            } else if ($formation == "GI") {
+                $subjectTag = match ($subjectTag) {
+                    "GIN" => "Gestion industrielle",
+                    "APS" => "Systèmes automatisés de production",
+                    "APM" => "Algorithmique, programmation et modélisation en UML",
+                    "ROO" => "Recherche Opérationnelle",
+                    "PSX" => "Probabilités, statistiques, plans d'expériences",
+                    "RDM" => "Résistance Des Matériaux",
+                    "PFI" => "Procédés de fabrication, industrialisation",
+                    "PSC" => "Penser système et cycle de vie",
+                    "COM" => "Théâtre Sciences humaines et Communication",
+                    "PPP" => "Projet Personnel Professionnel",
+                    "LV1" => "Langue Vivante 1",
+                    "LV2" => "Langue Vivante 2",
+                    "EPS" => "Éducation Physique et Sportive",
+                    "*" => $subject,
+                    default => $subjectTag,
+                };
+            } else if ($formation == "HU") {
+                // matières des humas
+                $subjectTag = match ($subjectTag) {
+                    "ALL" => "Allemand",
+                    "ANG" => "Anglais",
+                    "ARA" => "Arabe",
+                    "CHI" => "Chinois",
+                    "ESP" => "Espagnol",
+                    "FLE" => "Français Langue Étrangère",
+                    "ITA" => "Italien",
+                    "JAP" => "Japonais",
+                    "POR" => "Portugais",
+                    "RUS" => "Russe",
+                    "TAN" => "Tandem",
+                    "*" => $subject,
+                    default => $subjectTag,
+                };
+
+        } elseif ($subjectTag == "P2I") {
             $subjectTag = "P2I" . $p2i_number . " - " . $classDetails;
         }
     }
 
-    $locationInSummary = $room && $room != 'false';
+    $locationInSummary = $room && $room != "false";
 
-    $event->summary = ($type == null ? "" : $type . " ") . $subjectTag . (($location == null || !$locationInSummary) ? "" : " - " . $location);
+    $event->summary =
+        ($type == null ? "" : $type . " ") .
+        $subjectTag .
+        ($location == null || !$locationInSummary ? "" : " - " . $location);
     $event->location = $fullLocation;
 
     printEvent($event);
@@ -162,38 +248,38 @@ function printEvent($event): void
 
 function getEventDataString($event): string
 {
-    $data = array(
-        'SUMMARY' => $event->summary,
-        'DTSTART' => $event->dtstart,
-        'DTEND' => $event->dtend,
+    $data = [
+        "SUMMARY" => $event->summary,
+        "DTSTART" => $event->dtstart,
+        "DTEND" => $event->dtend,
         /*'DTSTART_TZ' => $event->dtstart_tz,
-        'DTEND_TZ' => $event->dtend_tz,*/
-        'DURATION' => $event->duration,
-        'DTSTAMP' => $event->dtstamp,
-        'UID' => $event->uid,
-        'CREATED' => $event->created,
-        'LAST-MODIFIED' => $event->last_modified,
-        'DESCRIPTION' => $event->description,
-        'LOCATION' => $event->location,
-        'SEQUENCE' => $event->sequence,
-        'STATUS' => $event->status,
-        'TRANSP' => $event->transp,
-        'ORGANISER' => $event->organizer,
-        'ATTENDEE(S)' => $event->attendee,
-    );
+         'DTEND_TZ' => $event->dtend_tz,*/
+        "DURATION" => $event->duration,
+        "DTSTAMP" => $event->dtstamp,
+        "UID" => $event->uid,
+        "CREATED" => $event->created,
+        "LAST-MODIFIED" => $event->last_modified,
+        "DESCRIPTION" => $event->description,
+        "LOCATION" => $event->location,
+        "SEQUENCE" => $event->sequence,
+        "STATUS" => $event->status,
+        "TRANSP" => $event->transp,
+        "ORGANISER" => $event->organizer,
+        "ATTENDEE(S)" => $event->attendee,
+    ];
 
     // Remove any blank values
     $data = array_filter($data);
 
-    $output = '';
+    $output = "";
     foreach ($data as $key => $value) {
         $output .= sprintf("%s:%s\r\n", $key, str_replace("\n", "\\n", $value));
     }
     return $output;
 }
 
-if (isset($_GET['url'])) {
-    convertCalendar(urldecode($_GET['url']), $_GET['mode'], $_GET['room']);
+if (isset($_GET["url"])) {
+    convertCalendar(urldecode($_GET["url"]), $_GET["mode"], $_GET["room"]);
 } else {
     header("Location: ./");
 }
