d8e57d804980e8bb22b5e5dd042960719a9bb84b
Aaabdou <101267176+Aaabdou@users.noreply.github.com>
2024-12-12 15:47:34 +0100

    attente tab added

---
+++ src/attente/index.php
@@ -0,0 +1,72 @@
+<?php
+require '../template/head.php';
+require '../template/header.php';
+require '../template/footer.php';
+require '../origin_path.php';
+require '../../libs/router.php';
+$name = "INS'Attente";
+$title = "Temps d'attente des restaurants";
+$desc = "Tiens toi au courant du temps d'attente du RI et de l'Olivier pour mieux t'organiser durant la pause.";
+
+$bde = isset($_GET['bde']);
+?>
+
+<!DOCTYPE html>
+<html lang="fr" class="<?= $bde ? 'bde' : '' ?>">
+<head>
+    <?php printHead($name, $title, $desc, '', 'attente/icons/icon-128.png', 'attente/attente.webmanifest') ?>
+    <link href="<?= getRootPath() ?>attente/main.css" rel="stylesheet"/>
+</head>
+<body class="<?= $bde ? 'bde' : '' ?>">
+<div class="csrf-container" data-csrf="<?= htmlspecialchars(gen_csrf_key('js')) ?>"></div>
+
+<?php if (!$bde) printHeader($name, $title); ?>
+<main class="<?= $bde ? 'bde' : '' ?>" id="app">
+    <?php if (!$bde) { ?>
+        <section id="rest-select">
+            <toggle-group
+                    label="Restaurant"
+                    :buttons="['Olivier', 'RI:Déjeuner', 'RI:Diner']"
+                    :disabled_indices="disabled_rest_indices"
+                    v-model:selected_index="ui.selected_rest_index"
+            ></toggle-group>
+        </section>
+    <?php } ?>
+
+    <section id="restaurants"
+             :set="restaurant = selected_restaurant">
+        <template v-if="!ui.week_menu_available">
+            <h2>Le menu de la semaine sera disponible à 11h10.</h2>
+        </template>
+        <template v-else-if="is_waitingTime_empty">
+            <h2>Le menu n'a pas encore été publié par les restaurants INSA.</h2>
+        </template>
+        <template v-else-if="restaurant">
+            <div class="waiting-content">
+                <div class="current-wait">
+                    <h2>Temps d'attente actuel (en minutes)</h2> 
+                    <p v-if = "restaurant.actualWaitingTime < 10">0{{ restaurant.actualWaitingTime }}</p>
+                    <p v-else>{{ restaurant.actualWaitingTime }}</p>
+                </div>
+                <div class="wait-prediction">
+                    <h2>Prédiction d'attente</h2>
+                    <div class="chart">
+                        <canvas id="histogramCanvas"></canvas>
+                    </div>
+                </div>
+                </div>
+        </template>
+        <template v-else>
+            <h2>Ce menu n'est pas disponible pour la date sélectionnée.</h2>
+        </template>
+    </section>
+</main>
+<footer>
+    <?= $bde ? getFooter('BdE INSA Lyon', "Powered by insa-utils.fr")
+        : getFooter('<a href="https://menu-restaurants.insa-lyon.fr" target="_blank">Page officielle</a>', "Clément GRENNERAT") ?>
+</footer>
+<script src="<?= getRootPath() ?>template/main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="<?= getRootPath() ?>attente/main.js" type="module"></script>
+</body>
+</html>
---
+++ src/attente/main.js
@@ -0,0 +1,212 @@
+import {createApp} from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js' // vue.esm-browser.prod.js
+import toggle_group from './toggle-group.js'
+
+const today_date = new Date();
+
+const REST_INDICES = {
+    OLIVIER_LUNCH: 0,
+    RI_LUNCH: 1,
+    RI_DINNER: 2,
+}
+
+// Scaling for BdE screens function
+function adjustFontSizeToFit() {
+    const html = document.documentElement;
+    if (!html.classList.contains('bde')) return;
+
+    let fontSize = 10; // Start font size in pixels
+    html.style.fontSize = `${fontSize}px`;
+
+    function increaseFontSize() {
+        if (
+            document.documentElement.scrollHeight <= window.innerHeight &&
+            document.documentElement.scrollWidth <= window.innerWidth
+        ) {
+            if(fontSize > 50) return; // Max font size
+
+            // Increment font size and apply it
+            fontSize += 0.5;
+            html.style.fontSize = `${fontSize}px`;
+
+            // Check again on the next frame
+            requestAnimationFrame(increaseFontSize);
+        } else {
+            // Backtrack by a small amount to fit within the viewport
+            fontSize -= 0.5;
+            html.style.fontSize = `${fontSize}px`;
+        }
+    }
+    // Start the resizing loop
+    increaseFontSize();
+}
+// Initial adjustment
+adjustFontSizeToFit();
+// Adjust font size on window resize
+window.addEventListener('resize', adjustFontSizeToFit);
+
+
+
+function get_default_selected_rest(disable_olivier = false){
+    const storage_value = localStorage.getItem('selected_rest_index');
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    if((storage_value == REST_INDICES.OLIVIER_LUNCH || urlParams.get('rest') === 'olivier') && !disable_olivier){
+        if(today_date.getDay() !== 0 && today_date.getDay() !== 6){ // Not weekend
+            return REST_INDICES.OLIVIER_LUNCH;
+        }
+    }
+
+    if(today_date.getHours() < 14 || today_date.getDay() === 6){ // Before 14h or sunday
+        return REST_INDICES.RI_LUNCH
+    }else return REST_INDICES.RI_DINNER
+}
+
+createApp({
+    data(){
+        return {
+            data: null,
+            ui: {
+                selected_day_index: today_date.getDay() === 0 ? 6 : today_date.getDay() - 1,
+                selected_rest_index: get_default_selected_rest(),
+                week_menu_available: true,
+            },
+        }
+    },
+    components: {
+        "toggle-group": toggle_group
+    },
+    computed: {
+        rest_id: function(){
+            if(this.ui.selected_rest_index === REST_INDICES.OLIVIER_LUNCH) return 'olivier';
+            return 'ri'
+        },
+        time_id: function(){
+            if(this.ui.selected_rest_index === REST_INDICES.RI_DINNER) return 'dinner';
+            return 'lunch'
+        },
+        disabled_rest_indices: function(){
+            let indices = [];
+            if(this.ui.selected_day_index >= 5){
+                indices.push(REST_INDICES.OLIVIER_LUNCH)
+                if(this.ui.selected_day_index === 5){
+                    indices.push(REST_INDICES.RI_DINNER)
+                }
+            }
+            return indices;
+        },
+        selected_restaurant: function(){
+            return this.data?.[this.rest_id]?.[this.time_id];
+        },
+        is_waitingTime_empty: function(){
+            const restaurant = this.selected_restaurant;
+            return !restaurant 
+        },
+    },
+    methods: {
+        renderHistogram() {
+            const canvas = document.getElementById('histogramCanvas');
+            if (!canvas) {
+                console.error("Canvas element not found!");
+                return;
+            }
+            const ctx = canvas.getContext('2d');
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: this.time_id === 'lunch' ? ["11:30", "11:40", "11:50", "12:00", "12:10", "12:20", "12:30", "12:40", "12:50", "13:00", "13:10", "13:20", "13:30"] : ["18:00", "18:10", "18:20", "18:30", "18:40", "18:50","19:00", "19:10", "19:20", "19:30"],
+                    datasets: [
+                        {
+                            label: 'Temps d\'attente prédictif',
+                            data: this.selected_restaurant?.["predictionTime"],
+                            backgroundColor: '#D32F2F',
+                            borderRadius: 5,
+                        },
+                    ],
+                },
+                options: {
+                    responsive: true,
+                    plugins: {
+                        legend: {
+                            display: false,
+                        },
+                    },
+                    scales: {
+                        x: {
+                            grid: {
+                                display: false,
+                            },
+                        },
+                        y: {
+                            beginAtZero: true,
+                            grid: {
+                                color: '#e0e0e0',
+                            },
+                        },
+                    },
+                },
+            });
+        },
+        get_day_buttons_names: function(){
+            let data = [];
+            const current_week_index = today_date.getDay() === 0 ? 6 : today_date.getDay() - 1;
+
+            for(let i = -current_week_index; i < 7 - current_week_index; i++){
+                const date = new Date(today_date.getFullYear(), today_date.getMonth(), today_date.getDate() + i)
+
+                let title = date.toLocaleDateString('fr', {weekday: 'short'});
+                title = title.substring(0, 1).toUpperCase() + title.substring(1, title.length - 1)
+                let subtitle = date.toLocaleDateString('fr', {month: 'short', day: 'numeric'}).toLowerCase();
+
+                data.push(title + ':' + subtitle);
+            }
+            return data;
+        },
+        get_dish_html: function(dish, prefix = false){
+            let html = '';
+
+            if(prefix){
+                html += `<span class="prefix">${prefix} :</span>`
+            }
+            return html;
+        },
+    },
+    watch: {
+        'ui.selected_day_index': function(new_selected_day_index){
+
+        },
+        'ui.selected_rest_index': function(new_selected_rest_index){
+            localStorage.setItem('selected_rest_index', new_selected_rest_index);
+        },
+        disabled_rest_indices: function(new_disabled_indices){
+            if(new_disabled_indices.includes(this.ui.selected_rest_index)){
+                const def = get_default_selected_rest(true)
+                if(new_disabled_indices.includes(def)){
+                    console.log(REST_INDICES.RI_LUNCH)
+                    this.ui.selected_rest_index = REST_INDICES.RI_LUNCH; // always available
+                }else{
+                    console.log(def)
+                    this.ui.selected_rest_index = def;
+                }
+            }
+        },
+    },
+    created(){
+        console.log("Fetching waiting data...")
+        fetch('attente/data/waitingTime.json', {cache: "no-store"})
+            .then(response => response.json())
+            .then(data => {
+                console.log("Waiting data fetched.", data)
+                this.data = data
+                this.$nextTick(() => {
+                    this.renderHistogram(); // Call histogram rendering after mount
+                })
+            });
+
+    },
+    // mounted(){
+    //     this.$nextTick(() => {
+    //         this.renderHistogram(); // Call histogram rendering after mount
+    //     })
+    // }
+}).mount('#app')
