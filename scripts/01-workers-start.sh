b8689432296162f245aaeb844a7896fccb0da78f
Clément Grennerat <clement.grennerat@free.fr>
2024-12-25 17:22:30 +0100

    Updating scripts

--- scripts/01-workers-start.sh
+++ scripts/01-workers-start.sh
@@ -4,7 +4,7 @@ echo "Installing python requirements for menu worker"
 pip install -r /var/www/html/workers/menu/requirements.txt
 
 echo "Reading VPN password for menu worker"
-echo "$VPN_PASSWORD" > /var/www/html/workers/menu/password.env
+echo "$VPN_PASSWORD" > password.env
 unset VPN_PASSWORD
 
 echo "Starting menu worker"
