c22922ccb1036e245a6a5b63e1795174e975da83
Clément Grennerat <clement.grennerat@free.fr>
2025-02-07 14:45:57 +0100

    [cal] Fixing empty subject on preg_match

--- src/cal/get.php
+++ src/cal/get.php
@@ -83,8 +83,11 @@ function format_name_from_regex_result($obj, $name_name, $details): string
  * @param $text string to match the regex with
  * @return bool true if the regex has matched or is invalid, false otherwise
  */
-function match_regex_and_update(&$object, $regex_name, $text): bool
+function match_regex_and_update(mixed &$object, string $regex_name, string $text): bool
 {
+    if (!isset($text) || $text == '') {
+        return false;
+    }
     if (!isset($object[$regex_name . '_regex'])) {
         $object['regex_' . $regex_name . '_matched'] = $text;
         return true;
