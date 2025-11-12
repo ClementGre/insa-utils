b31473c6dbc2fd687bfa5495415c55535305df7c
Clément Grennerat <clement.grennerat@clgr.io>
2025-11-12 19:06:22 +0100

    Setup meilisearch connexion & migration

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sleep 2
+
 echo "Running migrations"
 
 while ! php ./migrations/migrate-update.php; do
@@ -8,3 +10,13 @@ while ! php ./migrations/migrate-update.php; do
 done
 
 echo "migrate-update.php completed successfully."
+
+
+echo "Migrating meilisearch indexes"
+
+while ! php ./migrations/migrate-meilisearch.php; do
+    echo "migrate-meilisearch.php crashed with exit code $?.  Restarting in 2s..." >&2
+    sleep 2
+done
+
+echo "migrate-meilisearch.php completed successfully."
--- public/link/php/db_meili_utils.php
+++ public/link/php/db_meili_utils.php
@@ -11,7 +11,7 @@ function addLink($author_id, $author_name, $expiration_date, $title, $descriptio
         ':link' => $link
     ]);
     if ($r) {
-        require_once __DIR__ . '/../php/meilisearch_connect.php';
+        require_once __DIR__ . '/../../../libs/meilisearch_connect.php';
         global $client;
 
         $document = [
@@ -41,7 +41,7 @@ function deleteLink($id, $author_id): bool
         ':author_id' => $author_id
     ]);
     if ($r && $q->rowCount() > 0){
-        require_once __DIR__ . '/../php/meilisearch_connect.php';
+        require_once __DIR__ . '/../../../libs/meilisearch_connect.php';
         global $client;
         $client->index('links')->deleteDocument($id);
     }
@@ -82,7 +82,7 @@ function editLink($author_id, $id, $expiration_date, $title, $description, $link
             $q->execute([':id' => $id]);
         }
 
-        require_once __DIR__ . '/../php/meilisearch_connect.php';
+        require_once __DIR__ . '/../../../libs/meilisearch_connect.php';
         global $client;
         $client->index('links')->updateDocuments([[
             'id' => $id,
@@ -107,7 +107,7 @@ function update_link_likes($link_id, $likes, $dislikes): bool
         ':link_id' => $link_id
     ]);
     if ($r) {
-        require_once __DIR__ . '/../php/meilisearch_connect.php';
+        require_once __DIR__ . '/../../../libs/meilisearch_connect.php';
         global $client;
         $client->index('links')->updateDocuments([
             [
@@ -118,93 +118,4 @@ function update_link_likes($link_id, $likes, $dislikes): bool
         ]);
     }
     return $r;
-}
-function recalculate_link_likes(): void
-{
-    $q = getDB()->prepare("SELECT id, author_id FROM links");
-    $q->execute();
-    $links = $q->fetchAll(PDO::FETCH_ASSOC);
-    foreach($links as $link){
-        $q = getDB()->prepare('SELECT COUNT(*) FROM links_likes WHERE link_id = :id AND type = \'like\' AND user_id != :author_id');
-        $q->execute([':id' => $link['id'], ':author_id' => $link['author_id']]);
-        $likes = $q->fetch()[0];
-
-        $q = getDB()->prepare('SELECT COUNT(*) FROM links_likes WHERE link_id = :id AND type = \'dislike\' AND user_id != :author_id');
-        $q->execute([':id' => $link['id'], ':author_id' => $link['author_id']]);
-        $dislikes = $q->fetch()[0];
-
-        update_link_likes($link['id'], $likes, $dislikes);
-    }
-}
-
-function synchronize_db_to_meili(): void
-{
-
-    $q = getDB()->prepare('SELECT * FROM links');
-    $q->execute();
-    require_once __DIR__ . '/../php/meilisearch_connect.php';
-    global $client;
-
-    $documents = array_map(function ($row) {
-        $q = getDB()->prepare('SELECT name from users WHERE id = ?');
-        $q->execute([$row['author_id']]);
-        $name = $q->fetch()[0];
-        $document = [
-            'id' => $row['id'],
-            'title' => $row['title'],
-            'description' => $row['description'],
-            'link' => $row['link'],
-            'author_name' => $name,
-            'likes' => $row['likes'],
-            'dislikes' => $row['dislikes'],
-        ];
-        if ($row['expiration_date'] && $expiration_date_epoch = strtotime($row['expiration_date'])) {
-            $document['expiration_date'] = $expiration_date_epoch;
-        }
-        return $document;
-    }, $q->fetchAll());
-
-    $client->index('links')->deleteAllDocuments();
-    $client->index('links')->addDocuments($documents);
-}
-
-function migrate_meilisearch(): void
-{
-    require_once __DIR__ . '/../php/meilisearch_connect.php';
-    global $client;
-
-    $client->index('links')->resetSettings();
-
-    // Results format
-    $client->index('links')->updateDisplayedAttributes([
-        'id'
-    ]);
-
-    // Search order
-    $client->index('links')->updateSearchableAttributes([
-        'title',
-        'description',
-        'link',
-        'author_name'
-    ]);
-
-    $client->index('links')->updateRankingRules([
-        "words",
-        "typo",
-        "proximity",
-        "attribute",
-        "sort",
-        "likes:desc",
-        "dislikes:asc",
-        "exactness",
-    ]);
-
-    $client->index('links')->updateDistinctAttribute('link');
-
-    // Filter
-
-    $client->index('links')->updateFilterableAttributes([
-        'expiration_date'
-    ]);
-
 }
