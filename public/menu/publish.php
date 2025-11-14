2779a6a3ac3d4d222561af882796983488680bfb
Clément Grennerat <clement.grennerat@clgr.io>
2025-11-14 13:36:39 +0100

    [menu] Creating menu directory if missing.

--- public/menu/publish.php
+++ public/menu/publish.php
@@ -3,6 +3,10 @@
 // Should be called with the get parameter "token=<MENU_PUSH_TOKEN>"
 
 if(isset($_GET['token']) && $_GET['token'] === getenv('MENU_PUSH_TOKEN')){
+    // Create a new directory/file if it doesn't exist yet
+    if(!is_dir(dirname(__FILE__) . '/data')){
+        mkdir(dirname(__FILE__) . '/data', 0755, true);
+    }
     // Read request json content and store it inside data/menu.json
     file_put_contents(dirname(__FILE__) . '/data/menu.json', file_get_contents('php://input'));
     // Copy data/menu.json to /data/menu.json to keep it persistent in case of server restart.
